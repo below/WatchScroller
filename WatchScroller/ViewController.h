47e910c177d3993e0be0627800bf3ad20a61da91
Alexander von Below <Alex@vonBelow.Com>
2015-05-08 12:01:29 +0200

    Initial Commit

---
+++ WatchScroller/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WatchScroller
+//
+//  Created by Alexander v. Below on 08.05.15.
+//  Copyright (c) 2015 Alexander von Below. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WatchScroller/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WatchScroller
+//
+//  Created by Alexander v. Below on 08.05.15.
+//  Copyright (c) 2015 Alexander von Below. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
