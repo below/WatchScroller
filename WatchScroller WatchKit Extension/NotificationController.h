d9b833f9f1fe2b82320f9f730817613fe0734f06
Alexander von Below <Alex@vonBelow.Com>
2015-05-08 12:54:34 +0200

    Initial Commit

---
+++ WatchScroller WatchKit Extension/NotificationController.h
@@ -0,0 +1,14 @@
+//
+//  NotificationController.h
+//  WatchScroller WatchKit Extension
+//
+//  Created by Alexander v. Below on 08.05.15.
+//  Copyright (c) 2015 Alexander von Below. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface NotificationController : WKUserNotificationInterfaceController
+
+@end
